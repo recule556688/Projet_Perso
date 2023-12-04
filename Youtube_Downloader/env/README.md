463452e10caa4c0443f85cc7e9a20c71eceb6a03
Karma <ledombre@gmail.com>
2023-12-04 05:28:11 +0100

    Initial push of the app

---
+++ Youtube_Downloader/env/README.md
@@ -0,0 +1,59 @@
+# 🎥 YouTube Downloader
+
+## 📚 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Prerequisites](#prerequisites)
+- [Installing](#installing)
+- [Usage](#usage)
+
+## 📖 About <a name = "about"></a>
+
+This project is a YouTube downloader built in Python using the pytube library and tkinter for the GUI. It allows users to download and save videos from YouTube. It also displays video information such as the author, publish date, views, and duration.
+
+## 🚀 Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine and start downloading Youtube Videos
+
+### 📋 Prerequisites <a name = "prerequisites"></a>
+
+You will need the following tools and packages to run this project:
+
+- Python 3.6 or later
+- pip (Python package installer)
+- charset-normalizer
+- certifi
+- tkinter
+- customtkinter
+- darkdetect
+- idna
+- packaging
+- Pillow
+- pytube
+- requests
+- urllib3
+
+You can install Python from the [official website](https://www.python.org/downloads/).
+
+pip is already installed if you are using Python 3 >=3.4 downloaded from python.org
+
+or if you are working in a Virtual Environment created by virtualenv or pyvenv.
+
+### 🔧 Installing <a name = "installing"></a>
+
+1. Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/username/youtube-downloader.git 
+cd Youtube_Downloader
+pip install -r requirements.txt
+python yt_downloader.py
+```
+### 🎬 Usage
+
+```py
+python yt_downloader.py
+
+# now copy paste a valid Youtube link in the GUI
+```
---
+++ Youtube_Downloader/env/yt_downloader.py
@@ -0,0 +1,168 @@
+import tkinter as tk
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+from pytube import YouTube, exceptions
+from customtkinter import (
+    CTk,
+    CTkLabel,
+    CTkEntry,
+    CTkProgressBar,
+    CTkButton,
+    set_appearance_mode,
+    set_default_color_theme,
+)
+
+# Constants
+WINDOW_GEOMETRY = "1600x900"
+IMAGE_SIZE = (500, int(500 / 16 * 9))
+
+# Our app window
+
+app = CTk()
+app.geometry(WINDOW_GEOMETRY)
+app.title("YouTube Downloader")
+
+def startDownload():
+    try:
+        # Disable the download button
+        download_button.configure(state="disabled")
+
+        ytlink = link.get()
+        ytObject = YouTube(ytlink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="red")
+        finish_label.configure(text="")
+
+        # Update video info labels
+        author_label.configure(text="Author: " + ytObject.author, text_color="red")
+        publish_date_label.configure(
+            text="Publish Date: " + str(ytObject.publish_date), text_color="red"
+        )
+        views_label.configure(text="Views: " + str(ytObject.views), text_color="red")
+
+        # Update video duration label
+        minutes, seconds = divmod(ytObject.length, 60)
+        duration_label.configure(
+            text=f"Duration: {minutes} minutes {seconds} seconds", text_color="red"
+        )
+
+        # Get the thumbnail URL
+        thumbnail_url = ytObject.thumbnail_url
+
+        # Download the image
+        response = requests.get(thumbnail_url)
+        img_data = response.content
+        img = Image.open(BytesIO(img_data))
+
+        # Resize the image to a larger fixed size
+        img = img.resize(IMAGE_SIZE, Image.LANCZOS)
+
+        # Convert the image to PhotoImage
+        photo_image = ImageTk.PhotoImage(img)
+
+        # Update the label with the image
+        thumbnail_label.configure(image=photo_image)
+        thumbnail_label.image = photo_image
+
+        video.download()
+        finish_label.configure(text="Download finished", text_color="green")
+
+        # Re-enable the download button
+        download_button.configure(state="normal")
+    except exceptions.PytubeError as e:
+        print(e)  # Print the exception
+        finish_label.configure(
+            text="Download failed: Invalid YouTube link", text_color="red"
+        )
+
+        # Re-enable the download button
+        download_button.configure(state="normal")
+    except Exception as e:
+        print(e)  # Print the exception
+        finish_label.configure(
+            text="Download failed: An unexpected error occurred", text_color="red"
+        )
+
+        # Re-enable the download button
+        download_button.configure(state="normal")
+
+
+# Initialize the pPercentage label with green color
+pPercentage = CTkLabel(app, text="0%")
+
+
+def on_progress(stream, _, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per + "%", foreground="green")
+    pPercentage.update()
+
+    # Update the progress bar
+    progressBar.set(float(percentage_of_completion) / 100)
+
+
+
+# Thumbnail label
+thumbnail_label = tk.Label(app, text="Thumbnail", fg="green")
+thumbnail_label.pack(padx=10, pady=10)
+
+
+# System settings
+
+set_appearance_mode("System")
+set_default_color_theme("green")
+
+
+# Adding ui elements
+
+title = CTkLabel(
+    app, text="Insert the link of the video you want to download", text_color="green"
+)
+title.pack(padx=10, pady=10)
+
+# Link input
+
+url_var = tk.StringVar()
+link = CTkEntry(app, width=600, height=50, textvariable=url_var, text_color="grey")
+link.pack(padx=10, pady=10)
+
+# Finish Downloading
+
+finish_label = CTkLabel(app, text="")
+finish_label.pack(padx=10, pady=10)
+
+# Progress pourcentage
+pPercentage = CTkLabel(app, text="0%")
+pPercentage.pack(padx=10, pady=10)
+
+progressBar = CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+# Download button
+
+download_button = CTkButton(
+    app, text="Download", command=startDownload, text_color="darkgreen"
+)
+download_button.pack(padx=10, pady=10)
+
+# Video info labels
+author_label = CTkLabel(app, text="")
+author_label.pack(padx=10, pady=10)
+
+publish_date_label = CTkLabel(app, text="")
+publish_date_label.pack(padx=10, pady=10)
+
+views_label = CTkLabel(app, text="")
+views_label.pack(padx=10, pady=10)
+
+duration_label = CTkLabel(app, text="")
+duration_label.pack(padx=10, pady=10)
+
+# Run the app
+
+app.mainloop()
