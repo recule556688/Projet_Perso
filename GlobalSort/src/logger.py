37412bb35abbd0ed2c2a51bdbd9d97f32a94fa09
Karma <ledombre@gmail.com>
2023-12-23 11:15:55 +0100

    Update logger.py to include additional logging levels

--- GlobalSort/src/logger.py
+++ GlobalSort/src/logger.py
@@ -3,7 +3,7 @@ import logging
 
 def setup_logging():
     logging.basicConfig(  # Configure the logging module
-        filename="file_sorter.log",
+        filename="User_Files/file_sorter.log",
         level=logging.INFO,
         format="%(asctime)s - %(message)s",
         datefmt="%d-%b-%y %H:%M:%S",
@@ -15,3 +15,11 @@ def log_message(level, message):
         logging.info(message)
     elif level == "error":
         logging.error(message)
+    elif level == "warning":
+        logging.warning(message)
+    elif level == "critical":
+        logging.critical(message)
+    elif level == "debug":
+        logging.debug(message)
+    else:
+        raise ValueError("Invalid level")
