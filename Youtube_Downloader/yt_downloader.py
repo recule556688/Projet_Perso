32b816f4ac8bc7e1095d50149830a84199ef23b1
Karma <ledombre@gmail.com>
2024-02-19 20:35:40 +0100

    Add import statements and set app icon

--- Youtube_Downloader/yt_downloader.py
+++ Youtube_Downloader/yt_downloader.py
@@ -1,4 +1,7 @@
+import sys
+import os
 import tkinter as tk
+from tkinter import font
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
@@ -9,26 +12,45 @@ from customtkinter import (
     CTkEntry,
     CTkProgressBar,
     CTkButton,
+    CTkFrame,
     set_appearance_mode,
     set_default_color_theme,
 )
+
+# Get the directory of the executable
+base_dir = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+
+# Construct the path to the icon file
+icon_path = os.path.join(base_dir, "app.ico")
 
 # Constants
 WINDOW_GEOMETRY = "1600x900"
 IMAGE_SIZE = (500, int(500 / 16 * 9))
 
 # Our app window
-
 app = CTk()
 app.geometry(WINDOW_GEOMETRY)
 app.title("YouTube Downloader")
+app.iconbitmap(icon_path)
+
+# Thumbnail label
+thumbnail_label = tk.Label(
+    app, text="Thumbnail", fg="green", borderwidth=3, relief="solid"
+)
+# Initially hide the label
+thumbnail_label.pack_forget()
 
 
 # Download function
 def startDownload():
     try:
         # Disable the download button
-        download_button.configure(state="disabled")
+        download_button.configure(
+            state="disabled",
+            text="Downloading...",
+            text_color="red",
+            font=("Helvetica", 12, "bold"),
+        )
 
         ytlink = link.get()
         ytObject = YouTube(ytlink, on_progress_callback=on_progress)
@@ -67,6 +89,9 @@ def startDownload():
         # Update the label with the image
         thumbnail_label.configure(image=photo_image)
         thumbnail_label.image = photo_image
+
+        # Show the label
+        thumbnail_label.pack(padx=10, pady=10)
 
         video.download()
         finish_label.configure(text="Download finished", text_color="green")
@@ -107,45 +132,48 @@ def on_progress(stream, _, bytes_remaining):
     progressBar.set(float(percentage_of_completion) / 100)
 
 
-# Thumbnail label
-thumbnail_label = tk.Label(app, text="Thumbnail", fg="green")
-thumbnail_label.pack(padx=10, pady=10)
-
 # System settings
-
 set_appearance_mode("System")
 set_default_color_theme("green")
 
-# Adding ui elements
-
-title = CTkLabel(
-    app, text="Insert the link of the video you want to download", text_color="green"
-)
-title.pack(padx=10, pady=10)
 
 # Link input
+link = CTkEntry(app, width=600, height=50, text_color="grey")
+link.insert(0, "Insert the link of the video you want to download")
 
-url_var = tk.StringVar()
-link = CTkEntry(app, width=600, height=50, textvariable=url_var, text_color="grey")
-link.pack(padx=10, pady=10)
+
+def clear_placeholder(event):
+    current = link.get()
+    if current == "Insert the link of the video you want to download":
+        link.delete(0, tk.END)
+
+
+link.bind("<FocusIn>", clear_placeholder)
+link.pack(padx=10, pady=50)
+
+# Title label
+title = CTkLabel(app, text="")
+title.pack(padx=10, pady=10)
 
 # Finish Downloading
-
 finish_label = CTkLabel(app, text="")
 finish_label.pack(padx=10, pady=10)
 
-# Progress pourcentage
-pPercentage = CTkLabel(app, text="0%")
-pPercentage.pack(padx=10, pady=10)
+# Progress percentage
+pPercentage = CTkLabel(app, text="0 %")
+pPercentage.pack(padx=10, pady=0)
 
 progressBar = CTkProgressBar(app, width=400)
 progressBar.set(0)
-progressBar.pack(padx=10, pady=10)
+progressBar.pack(padx=10, pady=5)
 
 # Download button
-
 download_button = CTkButton(
-    app, text="Download", command=startDownload, text_color="darkgreen"
+    app,
+    text="Download",
+    command=startDownload,
+    text_color="darkgreen",
+    font=("Helvetica", 12, "bold"),
 )
 download_button.pack(padx=10, pady=10)
 
@@ -162,6 +190,6 @@ views_label.pack(padx=10, pady=10)
 duration_label = CTkLabel(app, text="")
 duration_label.pack(padx=10, pady=10)
 
-# Run the app
 
+# Run the app
 app.mainloop()
